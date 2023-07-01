1dd9539342bf784a8e217f4949b3a0bc27d9c199
JohnHerdling <johnherdling@gmail.com>
2023-07-01 18:33:32 +0200

    created spaceship and move function

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ class Game: #initialization screen
         pygame.display.set_caption("Space Invaders")
         self.clock = pygame.time.Clock()
         self.running = True
-        self.background_img=pygame.image.load("spr_space_himmel.png")
+        self.spaceship = Spaceship (self, 370, 515)
+        self.background_img = pygame.image.load("spr_space_himmel.png")
 
 
         while self.running: #standby screen and game loob (alle permanet gezeigten Objekte)
@@ -19,7 +20,37 @@ class Game: #initialization screen
                 if event.type == pygame.QUIT:
                     self.running = False
 
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.spaceship.move(-10)
+                    if event.key == pygame.K_RIGHT:
+                        self.spaceship.move(10)
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.spaceship.move(10)
+                    if event.key == pygame.K_RIGHT:
+                        self.spaceship.move(-10)
+
+            self.spaceship.update()
             pygame.display.update()
+
+
+class Spaceship:
+    def __init__(self, game,x,y):
+        self.x=x
+        self.y=y
+        self.change_x=0
+        self.game=game
+        self.spaceship_img= pygame.image.load("spr_spaceship.png")
+
+    def move(self, speed):
+        self.change_x+=speed
+
+    def update(self):
+        self.x += self.change_x
+        self.game.screen.blit(self.spaceship_img, (self.x , self.y ))
+
 
 if __name__=="__main__":
     game = Game(800, 600)
