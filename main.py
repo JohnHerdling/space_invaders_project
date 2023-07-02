4b3dae806ee2298647995c5a50b9b5d4d05d6a19
JohnHerdling <johnherdling@gmail.com>
2023-07-02 11:13:51 +0200

    created bullets

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ class Game: #initialization screen
                         self.spaceship.move(-10)
                     if event.key == pygame.K_RIGHT:
                         self.spaceship.move(10)
+                    if event.key == pygame.K_SPACE:
+                        self.spaceship.fire_bullet()
 
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_LEFT:
@@ -33,6 +35,13 @@ class Game: #initialization screen
                         self.spaceship.move(-10)
 
             self.spaceship.update()
+            if len(self.spaceship.bullets) > 0:
+                for bullet in self.spaceship.bullets:
+                    if bullet.is_fired == True:
+                        bullet.update()
+                    else:
+                        self.spaceship.bullets.remove(bullet)
+
             pygame.display.update()
 
 
@@ -43,14 +52,42 @@ class Spaceship:
         self.change_x=0
         self.game=game
         self.spaceship_img= pygame.image.load("spr_spaceship.png")
+        self.bullets = []
+
+    def fire_bullet(self):
+        self.bullets.append(Bullet(self.game, self.x, self.y))
+        self.bullets[len(self.bullets) -1].fire()
 
     def move(self, speed):
         self.change_x+=speed
 
     def update(self):
         self.x += self.change_x
+        if self.x < 0:
+            self.x = 0
+        elif self.x > 736:
+            self.x = 736
         self.game.screen.blit(self.spaceship_img, (self.x , self.y ))
 
 
+class Bullet:
+    def __init__(self, game, x,y):
+        self.x = x
+        self.y = y
+        self.game = game
+        self.is_fired = False
+        self.bullet_speed = 10
+        self.bullet_img = pygame.image.load("spr_patrone.png")
+
+    def fire (self):
+         self.is_fired=True
+
+    def update(self):
+        self.y -= self.bullet_speed
+        if self.y <= 0:
+            self.is_fired = False
+        self.game.screen.blit(self.bullet_img, (self.x, self.y))
+
 if __name__=="__main__":
     game = Game(800, 600)
+    print(len(game.spaceship.bullets))
